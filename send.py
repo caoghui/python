d9adffe01d699291534ee1bc3b3da89fca8c6310
Bruce Cao <bruce.cao@vantagefx.com.cn>
2017-12-11 09:14:47 +0800

    update

--- send.py
+++ send.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
+import sys
 import pika
+
 connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
 channel = connection.channel()
 channel.queue_declare(queue='hello')
 
-channel.basic_publish(exchange='', routing_key='hello', body='Hello World')
+message = ' '.join(sys.argv[1:]) or "Hello World"
+channel.basic_publish(exchange='', routing_key='hello', body=message)
 
-print(" [x] send 'Hello World'")
+print(" [x] send %r" % (message, ))
 connection.close()
